992baf00427ecae5b841100d60a6c0f6b03cfad2
karinatech <karinaminty@gmail.com>
2020-10-17 11:54:07 -0400

    Tightly coupled

---
+++ Spring-00-LoosselyCoupled/src/App.java
@@ -0,0 +1,14 @@
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class App {
+    public static void main(String[] args) {
+        FullTimeMentor fullTimeMentor= new FullTimeMentor();
+        PartTimeMentor partTimeMentor= new PartTimeMentor();
+        MentorAccount mentorAccount=new MentorAccount(fullTimeMentor);
+        MentorAccount mentorAccount1=new MentorAccount(partTimeMentor);
+        mentorAccount.manageAccount();
+        mentorAccount1.manageAccount();
+    }
+}
