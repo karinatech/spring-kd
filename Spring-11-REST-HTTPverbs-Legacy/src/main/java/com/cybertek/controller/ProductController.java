70ad16a012719056cb8d9af68ec9edf79620521c
karinatech <karinaminty@gmail.com>
2021-01-26 19:07:34 -0500

    Rest API

--- Spring-11-REST-HTTPverbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-REST-HTTPverbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.cybertek.controller;
 import com.cybertek.entity.Product;
 import com.cybertek.service.ProductService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +19,21 @@ public class ProductController {
         return productService.getProduct(id);
     }
     @RequestMapping(value = "/products")
-    public @ResponseBody List<Product>geetProducts(){
+    public @ResponseBody List<Product>getProducts(){
         return productService.getProducts();
     }
+    @RequestMapping(value = "/products/delete/{id}",method = RequestMethod.DELETE)
+    public @ResponseBody List<Product>deleteProduct(@PathVariable("id") Long id){
+        return productService.delete(id);
+    }
+    @RequestMapping(value = "/products",method = RequestMethod.POST)
+    public @ResponseBody List<Product>createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+
+    }
+    @RequestMapping(value = "/products/{id}",method = RequestMethod.PUT)
+    public @ResponseBody List<Product>updateProoduct(@PathVariable("id") Long id,@RequestBody Product product){
+        return productService.updateProduct(id,product);
+    }
+
 }
